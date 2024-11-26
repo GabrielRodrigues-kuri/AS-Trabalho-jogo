364db4f7e0036799eef1cb312789894a199c889e
Gabriel <bielalme@gmail.com>
2024-11-26 12:40:14 -0300

    Add files via upload

---
+++ Estaodejogos/Estaodejogos/app/src/main/java/com/example/estaodejogos/Jogo.java
@@ -0,0 +1,14 @@
+package com.example.estaodejogos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Jogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_jogo);
+    }
+}
